f2be122851f2609616e7c212cbca5e5c8dc67bea
xhl <17854217062@163.com>
2026-07-02 06:04:39 +0800

    chore: 移除超出大小限制的 PDF 文件

---
+++ src/main/java/com/xhl/C06_Algorithm_Skills/ODExam/A177/YouKnow.md
@@ -0,0 +1,37 @@
+# 题型汇总
+    按照算法的难度和频率大致可以分为必会和进阶两种类型，每个子项排序表示考察的频率，序号越低，
+    考察频率越高，比如数组是考察频率最高的。大家实际刷题中可以根据题库提供的功能筛选，按照热度、题解数等进行筛选。
+## 必会
+       1. 数组  Array
+       2. 字符串 String
+       3. 排序 Sort
+       4. 贪心 Greedy
+       5. 递归 recursion 
+       6. 循环 Loop
+       7. 滑窗  slide window
+       8. 栈 Stack
+       9. 进制转换 base conversion
+       10. 位运算 bitwise operation
+       11. 队列 Queue
+       12. 哈希表 HashMap
+       13. 链表 LinkedList
+       14. 线性表 ArrayList
+       15. 二分查找 BinarySearch
+
+##  进阶
+       1. 图 Graph
+       2. 树 Tree
+       3. DFS搜索 DFS Search
+       4. BFS搜索 BFS Search
+       5. 动态规划 Dynamic programming
+       6. 前缀和  prefix sum
+       7. 排列组合  permutations and combinations
+       8. 矩阵  matrix 
+       9. 双指针 two pointers 
+       10. 回溯 backtracking 
+       11. 状态机 state machine
+       12. 并查集 Union-Find 
+       13. 正则表达式 regex
+       14. 分治 divide and conquer
+       15. 枚举 Enum
+       16. 统计 statistics
---
+++ src/main/java/com/xhl/C06_Algorithm_Skills/ODExam/TestPaperA/answer/A100/mergingPort.java
@@ -0,0 +1,100 @@
+package com.xhl.C08_Career_Growth.ODExam.TestPaperA.answer.A100;
+
+import java.util.*;
+
+/**
+ * @Author: xhl
+ * @Date: 2026-06-25 21:58
+ * @Description: 06 端口合并 mergingPort
+ */
+public class mergingPort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = Integer.parseInt(sc.nextLine());
+
+        // M,N不在限定范围内，统一输出一组空数组[[]]
+        if (m > 10 || m < 1) {
+            System.out.println("[[]]");
+            return;
+        }
+
+        // 这里使用ArrayList接收端口组，是为了后面更方便进行端口组之间的合并
+        ArrayList<ArrayList<Integer>> ports = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            Integer[] tmp =
+                    Arrays.stream(sc.nextLine().split(",")).map(Integer::parseInt).toArray(Integer[]::new);
+
+            // M,N不在限定范围内，统一输出一组空数组[[]]
+            int n = tmp.length;
+            if (n < 1 || n > 100) {
+                System.out.println("[[]]");
+                return;
+            }
+
+            ArrayList<Integer> tmpList = new ArrayList<>(Arrays.asList(tmp));
+            ports.add(tmpList);
+        }
+
+        System.out.println(getResult(ports));
+    }
+
+    // 算法入口
+    public static String getResult(ArrayList<ArrayList<Integer>> ports) {
+        outer:
+        while (true) {
+            // 这里倒序遍历端口组是为了实现：组外顺序保持输入顺序
+            for (int i = ports.size() - 1; i >= 0; i--) {
+                for (int j = i - 1; j >= 0; j--) {
+
+                    // 判断两个端口是否可以合并
+                    if (canUnion(ports.get(i), ports.get(j))) {
+                        // 将后面的端口组，并入前面的端口组，这样就不会破坏组外顺序
+                        ports.get(j).addAll(ports.get(i));
+                        ports.remove(i);
+                        continue outer;
+                    }
+                }
+            }
+
+            break;
+        }
+
+        StringJoiner out = new StringJoiner(",", "[", "]");
+        for (ArrayList<Integer> port : ports) {
+            StringJoiner in = new StringJoiner(",", "[", "]");
+            for (Integer v : new TreeSet<Integer>(port)) { // 这里使用TreeSet是为了实现：组内相同端口仅保留一个，从小到达排序。
+                in.add(v + "");
+            }
+            out.add(in.toString());
+        }
+
+        return out.toString();
+    }
+
+    // 如果端口组间存在2个及以上不同端口相同，则认为这2个端口组互相关联，可以合并。
+    // 下面方法实现中：对于“不同端口”的理解是：端口位置不同，端口值可以相同，即以不同位置的端口视为不同端口
+    public static boolean canUnion(ArrayList<Integer> port1, ArrayList<Integer> port2) {
+        port1.sort((a, b) -> a - b);
+        port2.sort((a, b) -> a - b);
+
+        int same = 0;
+        int i = 0;
+        int j = 0;
+
+        while (i < port1.size() && j < port2.size()) {
+            if (port1.get(i) - port2.get(j) == 0) {
+                i++;
+                j++;
+                if (++same >= 2) return true;
+            } else if (port1.get(i) > port2.get(j)) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return false;
+    }
+}
