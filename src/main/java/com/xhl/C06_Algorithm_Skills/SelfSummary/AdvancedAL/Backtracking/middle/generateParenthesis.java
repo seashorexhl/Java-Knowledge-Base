72fb7361696f3c683378fb079fbbe93bd15685b5
xhl <17854217062@163.com>
2026-07-02 15:40:00 +0800

    changes
    Former-commit-id: b54680de045a3dee3ea61ac20c8a7318183332b3

---
+++ src/main/java/com/xhl/C06_Algorithm_Skills/SelfSummary/AdvancedAL/Backtracking/middle/generateParenthesis.java
@@ -0,0 +1,20 @@
+package com.xhl.C06_Algorithm_Skills.SelfSummary.AdvancedAL.Backtracking.middle;
+
+import java.util.List;
+
+/**
+ * @Author: xhl
+ * @Date: 2026-07-02 14:54
+ * @Description:    22. 括号生成
+ */
+public class generateParenthesis {
+    static void main() {
+
+    }
+
+    public List<String> generateParenthesis(int n) {
+
+
+        return null;
+    }
+}
