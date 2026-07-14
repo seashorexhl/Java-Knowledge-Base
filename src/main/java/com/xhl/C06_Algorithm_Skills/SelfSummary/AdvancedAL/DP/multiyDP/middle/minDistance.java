6013c42afacec8973ed35c8a431f2a11c94ea71e
xhl <17854217062@163.com>
2026-07-14 10:17:59 +0800

    changes develop 7.14

--- src/main/java/com/xhl/C06_Algorithm_Skills/SelfSummary/AdvancedAL/DP/multiyDP/middle/minDistance.java
+++ src/main/java/com/xhl/C06_Algorithm_Skills/SelfSummary/AdvancedAL/DP/multiyDP/middle/minDistance.java
@@ -4,13 +4,44 @@ package com.xhl.C06_Algorithm_Skills.SelfSummary.AdvancedAL.DP.multiyDP.middle;
  * @Author: xhl
  * @Date: 2026-07-02 02:05
  * @Description:    72. 编辑距离
+ *  将 word1 转换成 word2 所使用的最少操作数
+ *  插入 删除 替换
  */
 public class minDistance {
     static void main() {
+        String word1 = "intention";
+        String word2 = "execution";
+        minDistance md = new minDistance();
+        int distance = md.minDistance(word1, word2);
+        System.out.println("所使用的最少操作数:" + distance);
+    }
+    /**
+     * 方法一：递归
+     */
+
+    // 大量的重复计算 会超时
+    public int minDistance(String word1, String word2) {
+        // 定义递归出口
+        if(word1.length() == 0 || word2.length() == 0){
+            return Math.max(word1.length(),word2.length());
+        }
+
+        if(word1.charAt(word1.length() - 1) == word2.charAt(word2.length() - 1)){
+            return  minDistance(word1.substring(0,word1.length()-1),word2.substring(0,word2.length()-1));
+
+        }
+        // 三个操作
+        return 1 + Math.min(minDistance(word1,word2.substring(0,word2.length()-1)),
+                Math.min(minDistance(word1.substring(0,word1.length()-1),word2),
+                minDistance(word1.substring(0,word1.length()-1),word2.substring(0,word2.length()-1))
+                ));
 
     }
-    //方法一：动态规划
-    public int minDistance(String word1, String word2) {
+    /**
+     *  方法二：动态规划
+     */
+    // 从状态转移 抽象出 状态转移方程
+    public int minDistance1(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
 
