b7867b91c9760ac177112a0106637e25f15adac4
xhl <17854217062@163.com>
2026-07-22 02:46:43 +0800

    changes develop 7.21 晚

--- src/main/java/com/xhl/C07_Algorithm_Skills/SelfSummary/BaseDS/Tree/BinaryTree/easy/getMinimumDifference.java
+++ src/main/java/com/xhl/C07_Algorithm_Skills/SelfSummary/BaseDS/Tree/BinaryTree/easy/getMinimumDifference.java
@@ -1,19 +1,28 @@
 package com.xhl.C07_Algorithm_Skills.SelfSummary.BaseDS.Tree.BinaryTree.easy;
 
 
-import com.xhl.C07_Algorithm_Skills.SelfSummary.BaseDS.Tree.TreeNode;
+import com.xhl.C07_Algorithm_Skills.SelfSummary.BaseDS.BinaryTree.BST.BSTreeNode;
 
 /**
  * @Author: xhl
  * @Date: 2026-06-22 12:31
  * @Description: 530. 二叉搜索树的最小绝对差
+ *
  */
 public class getMinimumDifference {
     static void main() {
+        BSTreeNode root = new BSTreeNode(4);
+        root.left = new BSTreeNode(2);
+        root.right = new BSTreeNode(6);
+        root.left.left = new BSTreeNode(1);
+        root.left.right = new BSTreeNode(3);
 
+        getMinimumDifference gmd = new getMinimumDifference();
+        int minimumDifference = gmd.getMinimumDifference(root);
+        System.out.println("Minimum Difference: " + minimumDifference);
     }
 
-    public int getMinimumDifference(TreeNode root) {
+    public int getMinimumDifference(BSTreeNode root) {
 
 
         return 0;
